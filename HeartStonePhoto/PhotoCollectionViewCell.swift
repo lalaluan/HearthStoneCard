caf1ff1c06d2b4a57b99dbdaa191a3fb6dee4ba3
RazielQAQ <raziel.qaq@gmail.com>
2016-05-24 15:13:08 +0800

    first commit

---
+++ HeartStonePhoto/PhotoCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCollectionViewCell.swift
+//  HeartStonePhoto
+//
+//  Created by nju on 16/5/24.
+//  Copyright © 2016年 CX. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var Image: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
+    
+}
